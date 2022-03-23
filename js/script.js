fe34752b36441b9b1d018d792ea363db280d7c68
Lucas-Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-03-23 17:10:41 +0000

    math

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Lucas Tyman All rights reserved
+//
+// Created by: Lucas Tyman
+// Created on: Mae @022
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  // this function does basic math
+document.getElementById('perimeter-math').innerHTML = '<p>The perimeter is= ' + (5 + 5 + 5 + 5) + '</p>'
+}
